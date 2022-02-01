25d8eefc36d6f37d672dfee76346f87bfb2790d6
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-02-01 13:04:34 +1000

    Created computerPlay function - working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello,World from script.js");
+//JS file for Rock Paper Scissors - The Odin Project
+
+function computerPlay(){
+    // randomly return rock, paper or scissors
+    let randomNo = Math.floor(Math.random()*3);
+    if (randomNo === 1){
+        return "rock";
+    } else if (randomNo === 2){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+computerPlay();
+
