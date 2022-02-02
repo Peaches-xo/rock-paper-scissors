541b4bf693ae526408f5e634f2c7b2661f2f1f00
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-02-03 08:38:08 +1000

    Completed game with ability to play 5 rounds

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ Goal - Phase 2:
   Ideas -> I would like to have closed fist icons/ images that bounce up and down similar to how you would move your hands playing IRL.
   On the third 'bounce', both choices would be revealed. The results would then be stored in a table to visually keep track of past choices 
   & who is winning. Bounce could be added with CSS animation/ keyframes but would need to look into this further. 
+
+
+Issues that came up & how I overcame them:
+  For a single round - 
+  Program would run but results were incorrect. Console would always log a tie even though both selections were different. Found playRound function was comparing playerSelection &
+  computerSelection as functions, hence my if statement was always returning "it's a tie" as they are both functions. I wanted the if statement to compare the values returned by the functions, not the functions themselves. 
+  I fixed this by adding () around the function expressions which returned the value to the corresponding variables, instead of returning the function. 
+
+  For playing multiple rounds - 
+  Code would run 5 times but only was logging results of the first round 5 times. Moved position of the loop 
+  to outside of the game function.
+
+  Console text was all the same, hard to visually differentiate where one round finished & the next started. 
+  Added some styling with %c to visually break up each round. Added fun rainbow text at the end to show the winner. 
--- script.js
+++ script.js
@@ -1,63 +1,87 @@
 //JS file for Rock Paper Scissors - The Odin Project
+let playerWins = 0;
+let compWins = 0;
 
-let playerSelection = prompt("Enter your selection:");
-playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-console.log (playerSelection);
+function game(){
+
+    //this function gets user input and returns in correct case. Calls function & logs selection. 
+    let playerSelection = (function(){
+    let input = prompt("enter your selection:");
+        input = input.slice(0,1).toUpperCase() + input.slice(1).toLowerCase();
+        return input;
+    })();
+    console.log("You selected: " + playerSelection);
+
+    // Step #3 - This function will randomly choose rock, paper or scissors and 
+    // return that value to variable computerSelection
+    let computerSelection = (function (){
+        let randomNo = Math.floor(Math.random()*3);
+        if (randomNo === 1){
+            return "Rock";
+        } else if (randomNo === 2){
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    })();
+    console.log("Computer selected: " + computerSelection);
+
+
+    //Step #4 - This function takes 2 parameters, then returns a string declaring the winner of the round
+    function playRound (playerSelection,computerSelection){
+        const scoreStyle = 'font-weight: bold; font-size: 20px;color: green';
+    
+        if (playerSelection === computerSelection) {
+            console.log( "%c It's a tie", scoreStyle);
+            
+
+            } else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+                (playerSelection == "Paper" && computerSelection == "Rock") ||
+                (playerSelection == "Scissors" && computerSelection == "Paper")) {
+                    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+                    playerWins = playerWins + 1;
+                    console.log("%c playerWins: " + playerWins + " | " + "compWins: " + compWins, scoreStyle);
+                     
+
+            } else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+                (playerSelection == "Paper" && computerSelection == "Scissors") ||
+                (playerSelection == "Scissors" && computerSelection == "Rock")){
+                    console.log( `You lose! ${computerSelection} beats ${playerSelection}`);
+                    compWins = compWins + 1;
+                    console.log("%c playerWins: " + playerWins + " | " + "compWins: " + compWins, scoreStyle);
+                    
+            }      
+    }
+    
  
- 
-
-
-let computerSelection = function computerPlay(){
-    // This function will randomly return rock, paper or scissors and put that value to computerSelection
-    let randomNo = Math.floor(Math.random()*3);
-    if (randomNo === 1){
-        return "Rock";
-    } else if (randomNo === 2){
-        return "Paper";
-    } else {
-        return "Scissors";
-    };
+    return(playRound(playerSelection,computerSelection));
 }
 
 
-    function playRound(playerSelection, computerSelection){
-        //this function compares players selection to computers selection and returns the results
-        switch(playerSelection){
-            case "Rock" :{
-                if (computerSelection === "Rock"){
-                    console.log("Computer Selected Rock - Its a tie!");
-                } else if (computerSelection ==="Paper"){
-                    console.log("Computer Selected Paper - You lose!");
-                } else {
-                    console.log("Computer Selected Scissors - You win!");
-                }
-                break;
-            }
-            case "Scissors":{
-                if (computerSelection === "Rock"){
-                    console.log("Computer Selected Rock - You lose!");
-                } else if (computerSelection ==="Paper"){
-                    console.log("Computer Selected Paper - You win!");
-                } else {
-                    console.log("Computer Selected Scissors - Its a tie!");
-                }
-                break;
-            } 
-            case "Paper":{
-                if (computerSelection === "Rock"){
-                    console.log("Computer Selected Rock - You win!");
-                } else if (computerSelection ==="Paper"){
-                    console.log("Computer Selected Paper - It's a tie");
-                } else {
-                    console.log("Computer Selected Scissors - You lose!");
-                }
-                break;
-            }
-            default: {
-                console.log("Not a valid weapon - please try again!");
-                break;
-            }
-        }
+let i = 0;
+let noOfTimes = 5;
+    while (i < noOfTimes){
+        game();
+        i++;
     }
-    
-playRound(playerSelection, computerSelection);
+
+if (i = noOfTimes) {
+if (playerWins > compWins){
+    const style = 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)';
+    console.log('%c You win!', style);
+} else {
+    const style = 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)';
+    console.log('%c You lose!', style);
+}
+
+    }
+
+
+
+
+
+
+
+
+
+
