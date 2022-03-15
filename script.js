d6d45b23609c0c503df822618c7c25cc04472877
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-03-15 21:03:27 +1000

    moved compSelection to play more than once. Updated & completed. All code working

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-//JS file for Rock Paper Scissors - The Odin Project
+//JS file for Rock Paper Scissors index.html - The Odin Project
 
-// Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
-
+// Create three buttons, one for each selection. 
 const rockBtn = document.querySelector("#btn_rock");
 const paperBtn = document.querySelector("#btn_paper");
 const scissorsBtn = document.querySelector("#btn_scissors");
+const img_left = document.querySelector(".img_left");
+const img_right = document.querySelector(".img_right");
 
+
+//Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked.
 rockBtn.addEventListener('click', function() { 
-    //start anim
-    //on anim end, change images to rock and comp selection
-    playRound("Rock",computerSelection); 
-}); 
+    img_left.classList.add('anim'); //add anim class to img_left 
+    img_right.classList.add('anim'); //add anim class to img_right
 
-paperBtn.addEventListener('click', function() { playRound("Paper",computerSelection); }); 
-scissorsBtn.addEventListener('click', function() { playRound("Scissors",computerSelection); }); 
-
-
-let playerWins = 0;
-let compWins = 0;
-
-
-// function game(){
-
-    //this function gets user input and returns in correct case. Calls function & logs selection. 
-    // let playerSelection = (function(){ //rps-ui dont need prompt to get input
-    // let input = prompt("enter your selection:");
-    //     input = input.slice(0,1).toUpperCase() + input.slice(1).toLowerCase();
-    //     return input;
-    // })();
-    // console.log("You selected: " + playerSelection);
-
-    // Step #3 - This function will randomly choose rock, paper or scissors and 
-    // return that value to variable computerSelection
-    let computerSelection = (function (){
+    let computerSelection = (function(){ //get compSelection
         let randomNo = Math.floor(Math.random()*3);
         if (randomNo === 1){
             return "Rock";
@@ -42,7 +23,75 @@ let compWins = 0;
             return "Scissors";
         }
     })();
-    console.log("Computer selected: " + computerSelection);
+
+    img_left.onanimationend = function(){  //when animation ends...
+        img_left.setAttribute('src', "../rock-paper-scissors/RockL.png"); //...change L img to selected 
+        img_right.setAttribute('src',`../rock-paper-scissors/${computerSelection}R.png`); //change R img
+        img_left.classList.remove('anim'); //..remove anim 
+        img_right.classList.remove('anim');
+        playRound("Rock",computerSelection); //call round
+    }
+    img_left.setAttribute('src', "../rock-paper-scissors/RockL.png"); //reset img back to rock
+    img_right.setAttribute('src', "../rock-paper-scissors/RockR.png");
+}); 
+
+paperBtn.addEventListener('click', function() { 
+    img_left.classList.add('anim'); //add anim class to img_left 
+    img_right.classList.add('anim'); //add anim class to img_right
+
+    let computerSelection = (function(){
+        let randomNo = Math.floor(Math.random()*3);
+        if (randomNo === 1){
+            return "Rock";
+        } else if (randomNo === 2){
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    })();
+
+    img_left.onanimationend = function(){ //when animation ends..
+        img_left.setAttribute('src', "../rock-paper-scissors/PaperL.png");
+        img_right.setAttribute('src',`../rock-paper-scissors/${computerSelection}R.png`); //change R img
+        img_left.classList.remove('anim');
+        img_right.classList.remove('anim');
+        playRound("Paper",computerSelection);
+    }
+    img_left.setAttribute('src', "../rock-paper-scissors/rockL.png");
+    img_right.setAttribute('src', "../rock-paper-scissors/RockR.png");
+ }); 
+
+scissorsBtn.addEventListener('click', function() {
+    img_left.classList.add('anim'); //add anim class to img_left 
+    img_right.classList.add('anim'); //add anim class to img_right
+
+    let computerSelection = (function(){
+        let randomNo = Math.floor(Math.random()*3);
+        if (randomNo === 1){
+            return "Rock";
+        } else if (randomNo === 2){
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    })();
+
+    img_left.onanimationend = function(){ //when animation ends... 
+        img_left.setAttribute('src', "../rock-paper-scissors/scissorsL.png");
+        img_right.setAttribute('src',`../rock-paper-scissors/${computerSelection}R.png`);
+        img_left.classList.remove('anim');
+        img_right.classList.remove('anim');
+        playRound("Scissors",computerSelection); 
+    }
+    img_left.setAttribute('src', "../rock-paper-scissors/rockL.png");
+    img_right.setAttribute('src', "../rock-paper-scissors/RockR.png");
+
+}); 
+
+let playerWins = 0;
+let compWins = 0;
+
+// function game(){
 
     const p_status = document.querySelector(".p_status");
     const p_playerScore = document.querySelector(".player_score");
@@ -51,10 +100,9 @@ let compWins = 0;
     //Step #4 - This function takes 2 parameters, then returns a string declaring the winner of the round
     function playRound (playerSelection,computerSelection){
         const scoreStyle = 'font-weight: bold; font-size: 20px;color: green';
-
+        console.log("Computer selected: " + computerSelection);
 
         if (playerSelection === computerSelection) {
-            // console.log( "%c It's a tie", scoreStyle);
             p_status.textContent = "It's a tie!";
 
             } else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
